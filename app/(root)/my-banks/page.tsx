dc1e3bc86c2af34ca21e61f6a7d60a779776dd6b
Dhrumit Savaiya <116923775+Dhrumit26@users.noreply.github.com>
2026-01-25 17:26:42 -0800

    Fix typo in my-banks page content

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,4 +1,4 @@
-hdfshygfuhiojdlsvguhdijnfdsvhudijdnsv hijknfsdc x
+hdfshygfuhiojdlsvguhdijnfdsvhudijdnsv hijknfsdcdsavsda
 import BankCard from '@/components/BankCard';
 import HeaderBox from '@/components/HeaderBox'
 import { getAccounts } from '@/lib/actions/bank.actions';
