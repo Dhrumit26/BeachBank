6841b0a99ffbbb8a1489fd4251201935b9046cea
Dhrumit Savaiya <116923775+Dhrumit26@users.noreply.github.com>
2026-01-25 17:51:29 -0800

    Fix typo in page.tsx

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,4 +1,4 @@
-hdfshygfuhiojdlsvguhdijnfdsvhudijdnsvhijknfsdcdsavsda
+hdfshygfuhiojdlsvguhdijnfdsvhudijdnsvhicjknfsdcdsavsda
 import BankCard from '@/components/BankCard';
 import HeaderBox from '@/components/HeaderBox'
 import { getAccounts } from '@/lib/actions/bank.actions';
