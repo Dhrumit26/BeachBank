49ba458d8de3d8ebbde2b6b647e55a298c2530d6
Dhrumit Savaiya <116923775+Dhrumit26@users.noreply.github.com>
2026-02-02 13:23:30 -0800

    Update page.tsx

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,3 +1,5 @@
+aaaaaaaaaasddddaegrfwdqsaf
+
 import HeaderBox from '@/components/HeaderBox'
 import PaymentTransferForm from '@/components/PaymentTransferForm'
 import { getAccounts } from '@/lib/actions/bank.actions';
